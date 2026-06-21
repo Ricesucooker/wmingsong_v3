d1fb53643b357a765ff2e28f04b36a49ab2e1838
Matcha  MacBook.air <pockynekokun@gmail.com>
2026-06-21 02:06:25 +0100

    start over

---
+++ wmingsong/src/components/Hero.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { heroMP4 } from '../utils/index.js'
+
+function Hero() {
+  return (
+    <div className="page-wf-hlvh flex">
+        <h1 className="items-center justify-center">div profile </h1>
+    </div>
+
+  );
+}
+
+export default Hero;
---
+++ wmingsong/src/utils/index.js
@@ -0,0 +1,66 @@
+//images import 
+
+//SVG
+import chibiSVG from "../assets/images/chibiTopsvg.svg";
+import iconGithub from "../assets/images/icons8-github.svg";
+import iconInsta from "../assets/images/icons8-instagram.svg";
+import iconLinkedin from "../assets/images/icons8-linkedin.svg";
+import iconEmail from "../assets/images/icons8-email.svg";
+import navBar from "../assets/images/icons8-menubar.svg";
+
+//Cert
+import certAWScpp from "../assets/images/aws-CPP.png";
+import certAWSdn from "../assets/images/aws-DA.png";
+import certAWSsa from "../assets/images/aws-SA.png";
+import certAzurefd from "../assets/images/MSazure-fund.png";
+
+
+//icons
+import aboutMeIcon from "../assets/images/aboutme.png"
+import photograIcon from "../assets/images/photography.png"
+import projectIcon from "../assets/images/projects.png"
+import recruiterIcon from "../assets/images/recruiter.png"
+
+//images
+import topPicture from "../assets/images/topSprings.jpg";
+import wMing from "../assets/images/wming.JPG";
+import photoOne from  "../assets/images/photo1.jpg";
+import photoTwo from  "../assets/images/photo2.jpg";
+import photoThree from  "../assets/images/photo3.jpg";
+import photoFour from  "../assets/images/photo4.jpg";
+import photoFive from  "../assets/images/photo5.jpg";
+import photoSix from  "../assets/images/photo6.jpg";
+import memeProejct from "../assets/images/oneprojectplease.jpg";
+
+//video
+import wmingMP4 from "../assets/video/WebHerovideo.mp4";
+
+export const heroMP4 = wmingMP4;
+
+export const topPic =topPicture;
+export const wmingJPG = wMing;
+export const imgOne = photoOne;
+export const imgTwo = photoTwo;
+export const imgThree = photoThree;
+export const imgFour = photoFour;
+export const imgFive = photoFive;
+export const imgSix = photoSix;
+export const memeImg = memeProejct;
+
+export const navSVG = navBar;
+export const chibiTopsvg = chibiSVG;
+export const githubIcon = iconGithub;
+export const instaIcon = iconInsta;
+export const linkedinIcon = iconLinkedin;
+export const emailIcon = iconEmail;
+
+export const iconMe = aboutMeIcon;
+export const iconRecrui = recruiterIcon;
+export const iconPhoto = photograIcon;
+export const iconProject = projectIcon;
+
+
+export const azureFD = certAzurefd;
+export const awsCPP = certAWScpp;
+export const awsSA = certAWSsa;
+export const awsDA = certAWSdn;
